7b77069e24586d0269e526be9a845efcd650c06a
taitodayo <mamapapadaisakusen@gmail.com>
2026-08-23 19:16:44 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ function swipeCard(direction) {
         resultText = '分かった';
     }
 
-    card.style.transition = 'transform 0.6s ease';
+    card.style.transition = 'transform 0.45s cubic-bezier(0.25,1,0,5,1)';
     card.style.transform = 'translateX(' + moveX + 'px) rotate(' + rotateDeg + 'deg)';
     card.style.opacity = '0'
 

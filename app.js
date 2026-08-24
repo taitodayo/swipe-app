44fdecf16bb085df1e70262f2022b04f7643d198
taitodayo <mamapapadaisakusen@gmail.com>
2026-08-24 13:42:29 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ function swipeCard(direction) {
         card.style.opacity = '0';
 
         setTimeout (() => {
-            card.style.transition = 'opacity 0.8s ease, transform 0.5s ease';
+            card.style.transition = 'opacity 0.4s ease, transform 0.4s ease';
             card.style.opacity = '1';
             card.style.transform = 'none'
         },50);
